07c3a60d151db97249a1cc1ae8b267dc997a8d16
Christian Stankowic <christian.stankowic@sva.de>
2025-06-12 14:18:11 +0200

    fix linting

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,9 +1,11 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-package provider
+package provider_test
 
 import (
+	"os"
+	"terraform-provider-uyuni/internal/provider"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -15,11 +17,16 @@ import (
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"uyuni": providerserver.NewProtocol6WithError(New("test")()),
+	"uyuni": providerserver.NewProtocol6WithError(
+		provider.New("test")(),
+	),
 }
 
 func testAccPreCheck(t *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
+	if v := os.Getenv("UYUNI_API_TOKEN"); v == "" {
+		t.Fatal("UYUNI_API_TOKEN must be set for acceptance tests")
+	}
 }
